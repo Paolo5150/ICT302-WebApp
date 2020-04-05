0ee870c7958379eaa32c138515bbf662d2fdb277
Paolo_X86 <p.ferri1986@gmail.com>
2020-04-05 13:57:32 +0800

    Re added body in email

--- server/email.php
+++ server/email.php
@@ -1,49 +1,42 @@
 <?php
 
-
+	require 'D:\Composer\vendor\autoload.php';
 	function sendEmail($to, $subject, $message)
 	{
 		mail($to,$subject,$message);
 	}
 	
 	use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+	use PHPMailer\PHPMailer\Exception;	
+
+	function sendEmailLocal($to, $subject,$message)
+	{		
+		$mail = new PHPMailer(TRUE);
+
+		try {
+
+				$mail->IsSMTP();                           // telling the class to use SMTP
+				$mail->SMTPAuth   = true;                  // enable SMTP authentication
+				$mail->Host       = "smtp.gmail.com"; // set the SMTP server
+				$mail->Port       = 587;                    // set the SMTP port
+				$mail->Username   = "ict302it07@gmail.com"; // SMTP account username
+				$mail->SMTPSecure = 'tsl';
+				$mail->Password   = "realtech_ict302";        // SMTP account password
+				$mail->From = "	ict302it07@gmail.com";
+				$mail->addAddress($to, "");
+				$mail->Subject = $subject;
+				$mail-> IsHTML(true);
+				$mail->Body = '<div></div><div>'.$message.'</div>';
+				$mail->send();
+				return true;
 
 
-require 'D:\Composer\vendor\autoload.php';
-
-function sendEmailLocal($to, $subject,$message)
-{
-	
-
-$mail = new PHPMailer(TRUE);
-
-try {
-
-		$mail->IsSMTP();                           // telling the class to use SMTP
-		$mail->SMTPAuth   = true;                  // enable SMTP authentication
-		$mail->Host       = "smtp.gmail.com"; // set the SMTP server
-		$mail->Port       = 587;                    // set the SMTP port
-		$mail->Username   = "ict302it07@gmail.com"; // SMTP account username
-		$mail->SMTPSecure = 'tsl';
-		$mail->Password   = "realtech_ict302";        // SMTP account password
-		$mail->From = "	ict302it07@gmail.com";
-		$mail->addAddress($to, "WHAT");
-		$mail->Subject = $subject;
-		$mail-> IsHTML(true);
-
-		$mail->send();
-		return true;
-
-
-		}
-		catch (Exception $e)
-		{
-		return false;
-		}
-
-
-
-}
+				}
+				catch (Exception $e)
+				{
+					echo 'Email error: ' .$e;
+					return false;
+				}
+	}
 
 ?>

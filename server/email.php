37273286c834daeb5216e448135a719b4e056bc6
Orangehawk <48203578+Orangehawk@users.noreply.github.com>
2020-04-06 16:29:07 +0800

    disabled part of email.php temporarily

--- server/email.php
+++ server/email.php
@@ -10,39 +10,39 @@
 	}
 
 
-	/* Namespace alias. */
-	use PHPMailer\PHPMailer\PHPMailer;
-	use PHPMailer\PHPMailer\Exception;
+// 	/* Namespace alias. */
+// 	use PHPMailer\PHPMailer\PHPMailer;
+// 	use PHPMailer\PHPMailer\Exception;
 
-	/* Include the Composer generated autoload.php file. */
-	require 'D:\composer\vendor\autoload.php';
+// 	/* Include the Composer generated autoload.php file. */
+// 	require 'D:\composer\vendor\autoload.php';
 
-	function sendEmailLocal($to, $subject,$message)
-	{
-		$mail = new PHPMailer(TRUE);
-		try {
+// 	function sendEmailLocal($to, $subject,$message)
+// 	{
+// 		$mail = new PHPMailer(TRUE);
+// 		try {
 
-		$mail->IsSMTP();                           // telling the class to use SMTP
-		$mail->SMTPAuth   = true;                  // enable SMTP authentication
-		$mail->Host       = "smtp.gmail.com"; // set the SMTP server
-		$mail->Port       = 587;                    // set the SMTP port
-		$mail->Username   = "ict302it07@gmail.com"; // SMTP account username
-		$mail->SMTPSecure = 'tsl';
-		$mail->Password   = "realtech_ict302";        // SMTP account password
-		$mail->From = "ict302it07@gmail.com";
-		$mail->addAddress($to, "WTF");
-		$mail->Subject = $subject;
-		$mail->Body = '<div></div><div>'.$message.'</div>';
-		$mail-> IsHTML(true);
+// 		$mail->IsSMTP();                           // telling the class to use SMTP
+// 		$mail->SMTPAuth   = true;                  // enable SMTP authentication
+// 		$mail->Host       = "smtp.gmail.com"; // set the SMTP server
+// 		$mail->Port       = 587;                    // set the SMTP port
+// 		$mail->Username   = "ict302it07@gmail.com"; // SMTP account username
+// 		$mail->SMTPSecure = 'tsl';
+// 		$mail->Password   = "realtech_ict302";        // SMTP account password
+// 		$mail->From = "ict302it07@gmail.com";
+// 		$mail->addAddress($to, "WTF");
+// 		$mail->Subject = $subject;
+// 		$mail->Body = '<div></div><div>'.$message.'</div>';
+// 		$mail-> IsHTML(true);
 
-// 		$mail->send();
-// 		return true;
+// // 		$mail->send();
+// // 		return true;
 
 
-		}
-		catch (Exception $e)
-		{
-		return false;
-		}
-	}
+// 		}
+// 		catch (Exception $e)
+// 		{
+// 		return false;
+// 		}
+// 	}
 ?>

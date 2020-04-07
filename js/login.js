c4de7b6b49e95ce760e7941685b856503b05d246
Orangehawk <48203578+Orangehawk@users.noreply.github.com>
2020-04-07 17:43:02 +0800

    Tidying code
    -Removed unused function
    -Removed commented code

--- js/login.js
+++ js/login.js
@@ -3,9 +3,7 @@ var password = document.getElementById("password"); //Password field
 var errortext = document.getElementById("errortext"); //errortext entry text
 var submit = document.getElementById("submit-btn"); //Form submit button
 
-var scriptTarget = "server/login.php?";
-
-//$("#errortext").hide();
+var scriptTarget = "server/login.php?"; //The login script location
 
 $(document).ready(function () {
 
@@ -20,11 +18,10 @@ $(document).ready(function () {
 
 function ValidateForm()
 {
-    errortext.innerHTML = "";
-    //$("#errortext").hide();
-
     var textDefaultColor = username.style.backgroundColor; //Default color for the text fields
     var submitDefaultColor = submit.style.backgroundColor; //Default color for the submit button
+
+    errortext.innerHTML = "";
 
     if (ValidateUsername(username.value) & ValidatePassword(password.value)) //If the form is valid
     {
@@ -32,24 +29,12 @@ function ValidateForm()
     }
     else //If the form is not valid
     {
-        
-        
-        
-
-        
-        setTimeout(function () //After 0.5 seconds, set the elements' colors back to normal
+        setTimeout(function () //After 0.5 seconds, set the elements' colors back to normal if they were changed by the validation functions
         {
             username.style.backgroundColor = textDefaultColor;
             password.style.backgroundColor = textDefaultColor;
             submit.style.backgroundColor = submitDefaultColor;
         }, 500);
-        
-        // $("#errortext").slideDown(1000);
-        // setTimeout(function () //After 3 seconds, remove the fail text
-        // {
-
-        //     $("#errortext").slideUp(1000);
-        // }, 4000);
     }
 }
 
@@ -99,10 +84,8 @@ function Login()
         Password: password.value
     }
 
-    errortext.innerHTML = "Logging in...";
+    errortext.innerHTML = "Logging in..."; //Let the user know the server is waiting
 
-    //$("#errortext").slideDown(1000);
-    
     DoPost(scriptTarget, myData, PostSuccess, PostFail);
 
     return true;
@@ -110,50 +93,15 @@ function Login()
 
 function PostSuccess(reply)
 {
-    console.log("Success" + reply);
     var obj = JSON.parse(reply);
 
     if(obj.Status == "fail")
         errortext.innerHTML = obj.Message;
     else if(obj.Status == "ok")
-        window.location = "test.html";
+        window.location = "test.html"; //Move to student portal
 }
 
 function PostFail(data, textStatus, errorMessage)
 {
     errortext.innerHTML = textStatus + ": " + errorMessage + ". Please try again or contact support.";
-
-    //$("#errortext").slideDown(1000);
-}
-
-function InvalidUser()
-{
-    invalid.innerHTML = "";
-    $("#invalid").hide();
-
-    var textDefaultColor = username.style.backgroundColor; //Default color for the text fields
-    var submitDefaultColor = submit.style.backgroundColor; //Default color for the submit button
-
-    username.select(); //Select the username again so the user can quickly change the name
-
-    invalid.innerHTML = "Invalid username or password<br/>" //Add fail text
-
-    username.style.backgroundColor = "red"; //Flash username field red
-    password.style.backgroundColor = "red"; //Flash password field red
-    submit.style.backgroundColor = "red"; //Flash submit button red
-
-    //$("#invalid").slideDown(1000);
-
-    setTimeout(function () //After 0.5 seconds, set the elements' colors back to normal
-    {
-        username.style.backgroundColor = textDefaultColor;
-        password.style.backgroundColor = textDefaultColor;
-        submit.style.backgroundColor = submitDefaultColor;
-    }, 500);
-
-    // setTimeout(function () //After 3 seconds, remove the fail text
-    // {
-    //     $("#invalid").slideUp(1000);
-    // }, 4000);
-
 }

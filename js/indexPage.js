1796465dfe2019765317cad4e733cb91d41a33ee
Paolo_X86 <p.ferri1986@gmail.com>
2020-04-11 14:29:10 +0800

    More fixes

--- js/indexPage.js
+++ js/indexPage.js
@@ -1,71 +1,80 @@
 $(document).ready(function () {
 
-        // Check local storage
-        var token = localStorage.getItem("Token");
-        var mus = localStorage.getItem("MurdochUserNumber");
+    // Check local storage
+    var token = localStorage.getItem("Token");
+    var mus = localStorage.getItem("MurdochUserNumber");
 
-        // and cookie, just in case
-        var cookie = document.cookie;
-        var cookieValues = cookie.split(";");
-        for(var i=0; i < cookieValues.length; i++)
+    // and cookie, just in case
+    var cookie = document.cookie;
+    var cookieValues = cookie.split(";");
+    for(var i=0; i < cookieValues.length; i++)
+    {
+        var cookieValue = cookieValues[i]
+
+        var index = cookieValue.indexOf("Token")
+        if(index != -1)       
         {
-            var cookieValue = cookieValues[i]
-
-            var index = cookieValue.indexOf("Token")
-            if(index != -1)       
+            if(token == null || token == "")
+            {
                 token = cookieValue.substr(cookieValue.indexOf("=") + 1, cookieValue.length)
-        
-
-            index = cookieValue.indexOf("MurdochUserNumber")
-            if(index != -1)
-                mus = cookieValue.substr(cookieValue.indexOf("=") + 1, cookieValue.length)
+            }
         }
+           
+    
 
-        var myData = {
-            Token: token,
-            MurdochUserNumber: mus
+        index = cookieValue.indexOf("MurdochUserNumber")
+        if(index != -1)
+        {
+            if(mus == null || mus == "")
+             mus = cookieValue.substr(cookieValue.indexOf("=") + 1, cookieValue.length)
         }
-
-        DoPost("server/getUserInfo.php",myData,(response)=>{
-                 var obj = JSON.parse(response)       
-                $("#welcome-title").html("Welcome " + obj.Data.FirstName)    
-                $("#main-content").html(obj.Data.TableContent)
-            },
-            (data, status, error)=>
-            {
-                alert("An error occurred")
-            } 
-        )
-
-      $("#logout-btn").click(function(e){
-
-        DoPost("server/logout.php",myData,(response)=>{
-
-            window.location = "index.php"   
-            },
-            (data, status, error)=>
-            {
-                alert("An error occurred")
-            } 
-        )
-        })
-})
-
-function onSessionButtonClicked(id)
-{
-    console.log(id)   
-    var myData = {
-        UserID: id,
-        SessionRequest: 1
     }
+
+    var myData = {
+        Token: token,
+        MurdochUserNumber: mus
+    }
+
     DoPost("server/getUserInfo.php",myData,(response)=>{
-        console.log(response)
-        var obj = JSON.parse(response)
-        $("#main-content").html(obj.Data.TableContent)   
+             var obj = JSON.parse(response)       
+            $("#welcome-title").html("Welcome " + obj.Data.FirstName)    
+            $("#main-content").html(obj.Data.TableContent)
         },
         (data, status, error)=>
         {
             alert("An error occurred")
         } 
     )
+
+  $("#logout-btn").click(function(e){
+
+    DoPost("server/logout.php",myData,(response)=>{
+
+        window.location = "index.php"   
+        },
+        (data, status, error)=>
+        {
+            alert("An error occurred")
+        } 
+    )
+    })
+})
+
+function onSessionButtonClicked(id)
+{
+console.log(id)   
+var myData = {
+    UserID: id,
+    SessionRequest: 1
+}
+DoPost("server/getUserInfo.php",myData,(response)=>{
+    console.log(response)
+    var obj = JSON.parse(response)
+    $("#main-content").html(obj.Data.TableContent)   
+    },
+    (data, status, error)=>
+    {
+        alert("An error occurred")
+    } 
+)
 }

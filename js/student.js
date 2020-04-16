3b56eb0010234276a77c98560fe74bacdbbce0e0
Paolo_X86 <p.ferri1986@gmail.com>
2020-04-16 18:13:20 +0800

    Added icons

--- js/student.js
+++ js/student.js
@@ -11,7 +11,7 @@ function buildSessionTable(response)
         <th>Date</th>
         <th>Start Time</th>
         <th>End Time</th>
-        <th>Retries</th>
+        <th>Errors</th>
     </tr>
     </thead>
     <tbody>
@@ -95,8 +95,30 @@ function Details(index)
     for(var i=0; i < arr[index][7].length; i++)
     {
         var logInd = "Log_" + i
-        if(logsObj[logInd] != undefined)        
-           logsStrings+= "<p>" + logsObj[logInd] + "</p>";
+        if(logsObj[logInd] != undefined)    
+        {
+            if(logsObj[logInd].indexOf("Failed") > 0)
+            {
+                logsStrings+= `<svg class="bi bi-x-circle-fill" width="3em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M16 8A8 8 0 110 8a8 8 0 0116 0zm-4.146-3.146a.5.5 0 00-.708-.708L8 7.293 4.854 4.146a.5.5 0 10-.708.708L7.293 8l-3.147 3.146a.5.5 0 00.708.708L8 8.707l3.146 3.147a.5.5 0 00.708-.708L8.707 8l3.147-3.146z" clip-rule="evenodd"/>
+                </svg>`
+            }
+            else if(logsObj[logInd].indexOf("Correctly") > 0)
+            {
+                logsStrings+= `<svg class="bi bi-check-circle" width="3em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M15.354 2.646a.5.5 0 010 .708l-7 7a.5.5 0 01-.708 0l-3-3a.5.5 0 11.708-.708L8 9.293l6.646-6.647a.5.5 0 01.708 0z" clip-rule="evenodd"/>
+                <path fill-rule="evenodd" d="M8 2.5A5.5 5.5 0 1013.5 8a.5.5 0 011 0 6.5 6.5 0 11-3.25-5.63.5.5 0 11-.5.865A5.472 5.472 0 008 2.5z" clip-rule="evenodd"/>
+              </svg>`
+            }         
+            else
+            {
+                logsStrings+= `<svg class="bi bi-caret-right" width="2em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M6 12.796L11.481 8 6 3.204v9.592zm.659.753l5.48-4.796a1 1 0 000-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 001.659.753z" clip-rule="evenodd"/>
+              </svg>`
+            }
+
+            logsStrings+= "<span>" + logsObj[logInd] + "</span><br/><br/>";
+        }
     }
 
     $("#details-" + index).html(logsStrings)  

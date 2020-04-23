10235e578f0145b3c3acb43d4a069147d2d78496
Paolo_X86 <p.ferri1986@gmail.com>
2020-04-23 19:41:33 +0800

    Added colors for report details

--- js/admin.js
+++ js/admin.js
@@ -319,9 +319,11 @@ function Details(index)
         {
             if(logsObj[logInd].indexOf("Failed") > 0)
             {
-                logsStrings+= `<svg class="bi bi-x-circle-fill" width="3em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                logsStrings+= `<svg class="bi bi-x-circle-fill" width="3em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style="font-color: red">
                 <path fill-rule="evenodd" d="M16 8A8 8 0 110 8a8 8 0 0116 0zm-4.146-3.146a.5.5 0 00-.708-.708L8 7.293 4.854 4.146a.5.5 0 10-.708.708L7.293 8l-3.147 3.146a.5.5 0 00.708.708L8 8.707l3.146 3.147a.5.5 0 00.708-.708L8.707 8l3.147-3.146z" clip-rule="evenodd"/>
                 </svg>`
+                logsStrings+= "<span style='font-size: 0.8em;color: red'>" + logsObj[logInd] + "</span><br/><br/>";
+
             }
             else if(logsObj[logInd].indexOf("Correctly") > 0)
             {
@@ -329,15 +331,18 @@ function Details(index)
                 <path fill-rule="evenodd" d="M15.354 2.646a.5.5 0 010 .708l-7 7a.5.5 0 01-.708 0l-3-3a.5.5 0 11.708-.708L8 9.293l6.646-6.647a.5.5 0 01.708 0z" clip-rule="evenodd"/>
                 <path fill-rule="evenodd" d="M8 2.5A5.5 5.5 0 1013.5 8a.5.5 0 011 0 6.5 6.5 0 11-3.25-5.63.5.5 0 11-.5.865A5.472 5.472 0 008 2.5z" clip-rule="evenodd"/>
               </svg>`
+                logsStrings+= "<span style='font-size: 0.8em;color: green'>" + logsObj[logInd] + "</span><br/><br/>";
+
             }         
             else
             {
                 logsStrings+= `<svg class="bi bi-caret-right" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M6 12.796L11.481 8 6 3.204v9.592zm.659.753l5.48-4.796a1 1 0 000-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 001.659.753z" clip-rule="evenodd"/>
               </svg>`
+                logsStrings+= "<span style='font-size: 0.8em'>" + logsObj[logInd] + "</span><br/><br/>";
+
             }
 
-            logsStrings+= "<span style='font-size: 0.8em'>" + logsObj[logInd] + "</span><br/><br/>";
         }    
     }
 
@@ -458,7 +463,6 @@ function CreateAdminAccount()
             } 
         )
       }
-
 }
 
 

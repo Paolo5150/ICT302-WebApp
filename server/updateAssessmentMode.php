8c06104b2f99ea661b33c5da34206339ac676d6b
Paolo_X86 <p.ferri1986@gmail.com>
2020-05-02 15:23:45 +0800

    Add AssessmentMode if not in table

--- server/updateAssessmentMode.php
+++ server/updateAssessmentMode.php
@@ -15,14 +15,9 @@
 
             $con = connectToDb();
 
-            //Prepare SQL statement. Place a '?' where you want to pass an argument
-            // Below I'm passing the student ID
-            $stmt = $con->prepare("select * from user where MurdochUserNumber = ?");
-                    
-            // This is where I'm passing the actual argument
+
+            $stmt = $con->prepare("select * from user where MurdochUserNumber = ?"); 
             $stmt->bind_param("s", $id);
-                    
-            // Execute the SQL statement!
             $stmt->execute();
                     
             //Check if we got something	
@@ -34,12 +29,27 @@
                 //Ensure It's admin
                 if($data['IsAdmin'] == 1)
                 {
-                    $stmt = $con->prepare("update Configuration set Value = ? where ConfigName = 'AssessmentMode'");
-                    $stmt->bind_param("s", $checked);
-                    $status = $stmt->execute();
+                    $stmt = $con->prepare("select Value from Configuration where ConfigName = 'AssessmentMode'"); 
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+                    if($result && $result->num_rows > 0)
+                    {
+                        $stmt = $con->prepare("update Configuration set Value = ? where ConfigName = 'AssessmentMode'");
+                        $stmt->bind_param("s", $checked);
+                        $status = $stmt->execute();
 
-                    $reply->Status = 'ok';
-                    $reply->Message = 'Assessment Mode is ' . (($checked == 'true') ? "on" : "off");
+                        $reply->Status = 'ok';
+                        $reply->Message = 'Assessment Mode is ' . (($checked == 'true') ? "on" : "off");
+                    }
+                    else
+                    {
+                        $stmt = $con->prepare("insert into Configuration (ConfigName,Value) VALUES ('AssessmentMode',?)");
+                        $stmt->bind_param("s", $checked);
+                        $status = $stmt->execute();
+
+                        $reply->Status = 'ok';
+                        $reply->Message = 'Assessment Mode is ' . (($checked == 'true') ? "on" : "off");
+                    }
                 }
             }
         }

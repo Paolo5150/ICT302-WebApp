ad20d7381fe2a7032082363fe47f46b8b1808761
Paolo_X86 <p.ferri1986@gmail.com>
2020-05-03 10:31:21 +0800

    Removed token from getconfig

--- server/getConfiguration.php
+++ server/getConfiguration.php
@@ -6,47 +6,36 @@
         //Prepare reply pbject
         $reply = new stdClass();
         $reply->Data = new stdClass();
-        if(isset($_POST['MurdochUserNumber']) && isset($_POST['Token']))
-        {
-            if(!IsAccountOK($_POST['MurdochUserNumber'], $_POST['Token'])) die();
 
-            $id = $_POST['MurdochUserNumber'];
 
-            $con = connectToDb();
+        $con = connectToDb();
 
-            //Prepare SQL statement. Place a '?' where you want to pass an argument
-            // Below I'm passing the student ID
-            $stmt = $con->prepare("select * from configuration");
-            $stmt->execute();
-            $result = $stmt->get_result();					
+        //Prepare SQL statement. Place a '?' where you want to pass an argument
+         // Below I'm passing the student ID
+        $stmt = $con->prepare("select * from configuration");
+        $stmt->execute();
+        $result = $stmt->get_result();					
                     
-            if($result && $result->num_rows > 0)
-            {			
-                $data = $result->fetch_all();
-
-                // Look for active layout
-                for($i=0 ; $i < count($data) ; $i++)
+        if($result && $result->num_rows > 0)
+        {			
+            $data = $result->fetch_all();
+            // Look for active layout
+            for($i=0 ; $i < count($data) ; $i++)
+            {
+                if($data[$i][0] == 'ActiveLayout')
                 {
-                    if($data[$i][0] == 'ActiveLayout')
-                    {
-                        $stmt = $con->prepare("select Value from layout where LayoutName = '{$data[$i][1]}'");
-                        $stmt->execute();
-                        $res = $stmt->get_result();
-                        $instruments = $res->fetch_assoc();
-                        $reply->Data->Layout = json_encode($instruments['Value']);
-                    }
-                    else if($data[$i][0] == 'AssessmentMode')
-                    {
+                    $stmt = $con->prepare("select Value from layout where LayoutName = '{$data[$i][1]}'");
+                    $stmt->execute();
+                    $res = $stmt->get_result();
+                    $instruments = $res->fetch_assoc();
+                    $reply->Data->Layout = json_encode($instruments['Value']);
+                }
+                else if($data[$i][0] == 'AssessmentMode')
+                {
                         $reply->Data->AssessmentMode = $data[$i][1];
-                    }
                 }
             }
-        }
-        else
-        {
-            $reply->Status= 'fail';
-            $reply->Message = "Missing info";
-        }
+            
 
         // Send reply in JSON format
         $myJSON = json_encode($reply);			
